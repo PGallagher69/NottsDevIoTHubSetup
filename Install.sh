f72e7faaa59b15a96ea64c7f7dd4d46078d3e74a
Peter Gallagher <pete@pjgcreations.co.uk>
2018-01-17 19:17:53 +0000

    Fix relative paths.

--- Install.sh
+++ Install.sh
@@ -2,24 +2,24 @@ sudo apt-get update
 sudo rpi-update 52241088c1da59a359110d39c1875cda56496764
 sudo apt-get install samba samba-common-bin -y
 echo -e "raspberry\nraspberry\n" | smbpasswd -s -a pi
-mkdir ~/share
+mkdir /home/pi/share
 #sudo nano /etc/samba/smb.conf
 cat SambaShare.txt >> /etc/samba/smb.conf
 smbd_restart
-cd ~/
+cd 
 git clone git://git.drogon.net/wiringPi
-cd wiringPi
+cd /home/pi/wiringPi
 ./build
 sudo apt-get remove nodered -y
 sudo apt-get remove nodejs -y
-cd ~/
+cd /home/pi
 curl -L https://git.io/n-install | bash -s -- -y 6.10.2
 . /home/pi/.bashrc
 node -v
 n=$(which node); n=${n%/bin/node}; chmod -R 755 $n/bin/*; sudo cp -r $n/{bin,lib,share} /usr/local
 sudo node -v
-cd ~/share
+cd /home/pi/share
 git clone -b start https://github.com/PGallagher69/NottsDevIoTHub.git
-cd NottsDevIoTHub
+cd /home/pi/NottsDevIoTHub
 npm install
 read -p "Installation Complete! Press any key to exit"
